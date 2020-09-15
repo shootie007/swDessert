1f99c41dc461c462a34c49915ee9ccc7cdfb995f
多田秀人 <shoot@Shootie-Air.local>
2020-09-15 15:12:43 +0900

    first commit

---
+++ MyDessert/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  MyDessert
+//
+//  Created by 多田秀人 on 2020/09/15.
+//  Copyright © 2020 多田秀人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UISearchBarDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        searchText.delegate = self
+        searchText.placeholder = "お菓子の名前を入力してください"
+    }
+
+
+    @IBOutlet weak var searchText: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        view.endEditing(true)
+        if let searchWord = searchBar.text {
+            print(searchWord)
+            searchDessert(keyword: searchWord)
+        }
+    }
+    
+    struct ItemJson: Codable {
+        let name: String?
+        let maker: String?
+        let url: URL?
+        let image: URL?
+    }
+    
+    struct ResultJson: Codable {
+        let item: [ItemJson]?
+    }
+    
+    func searchDessert(keyword : String) {
+        guard let keyword_encode = keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            return
+        }
+        
+        guard let req_url = URL(string:
+            "https://sysbird.jp/toriko/api/?apikey=guest&format=json&keyword=\(keyword_encode)&max=10&order=r") else {
+                return
+        }
+        print(req_url)
+        
+        let req = URLRequest(url: req_url)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: req, completionHandler: {
+            (data, response, error) in
+            session.finishTasksAndInvalidate()
+            do {
+                let decoder = JSONDecoder()
+                let json = try decoder.decode(ResultJson.self, from: data!)
+                
+                print(json)
+            } catch {
+                print("エラーが出ました")
+            }
+        })
+        
+        task.resume()
+    }
+}
+
