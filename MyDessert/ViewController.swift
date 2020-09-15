0edf913301d232c5b509c4869d1abf66caf2c303
多田秀人 <shoot@Shootie-Air.local>
2020-09-15 17:15:50 +0900

    added view on Safari.

--- MyDessert/ViewController.swift
+++ MyDessert/ViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import SafariServices
 
-class ViewController: UIViewController, UISearchBarDelegate {
+class ViewController: UIViewController, UISearchBarDelegate, UITableViewDataSource, UITableViewDelegate, SFSafariViewControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,11 +17,15 @@ class ViewController: UIViewController, UISearchBarDelegate {
         
         searchText.delegate = self
         searchText.placeholder = "お菓子の名前を入力してください"
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
 
     @IBOutlet weak var searchText: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
+    
+    var dessertList : [(name:String, maker:String, link:URL, image:URL)] = []
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         view.endEditing(true)
@@ -61,13 +66,54 @@ class ViewController: UIViewController, UISearchBarDelegate {
                 let decoder = JSONDecoder()
                 let json = try decoder.decode(ResultJson.self, from: data!)
                 
-                print(json)
+//                print(json)
+                
+                if let items = json.item {
+                    self.dessertList.removeAll()
+                    for item in items {
+                        if let name = item.name, let maker = item.maker, let link = item.url, let image = item.image {
+                            let dessert = (name, maker, link, image)
+                            self.dessertList.append(dessert)
+                        }
+                    }
+                    
+                    self.tableView.reloadData()
+                    if let dessertdbg = self.dessertList.first {
+                        print("---------------------------")
+                        print("dessertList[0] = \(dessertdbg)")
+                    }
+                }
+
             } catch {
                 print("エラーが出ました")
             }
         })
-        
         task.resume()
+    }
+        
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section : Int) -> Int {
+        return dessertList.count
+    }
+    
+    func tableView(_ tableView:UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "dessertCell", for: indexPath)
+        cell.textLabel?.text = dessertList[indexPath.row].name
+        if let imageData = try? Data(contentsOf: dessertList[indexPath.row].image) {
+            cell.imageView?.image = UIImage(data: imageData)
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let safariViewController = SFSafariViewController(url: dessertList[indexPath.row].link)
+        safariViewController.delegate = self
+        present(safariViewController, animated: true, completion: nil)
+    }
+    
+    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+        dismiss(animated: true, completion: nil)
     }
 }
 
